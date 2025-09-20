8d4800bda37b867b8479f4d1d84fa079f1788753
Trevor Chan <trev@trevrosa.dev>
2025-09-20 20:23:03 +0800

    change constant name

--- src/ble.rs
+++ src/ble.rs
@@ -2,9 +2,9 @@ use trouble_host::{Address, Controller, HostResources, prelude::DefaultPacketPoo
 
 const MAX_CONNECTIONS: usize = 2;
 
-const L2CAP_CHANNELS_MAX: usize = 2;
+const MAX_L2CAP_CHANNELS: usize = 2;
 
 pub fn peripheral(controller: impl Controller, address: [u8; 6]) {
-    let mut resources: HostResources<DefaultPacketPool, MAX_CONNECTIONS, L2CAP_CHANNELS_MAX> =
+    let mut resources: HostResources<DefaultPacketPool, MAX_CONNECTIONS, MAX_L2CAP_CHANNELS> =
         HostResources::new();
 }
